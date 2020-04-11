6929d41912215dbe4621ea7a7578403f0e3542cf
David J. H. Shih <djh.shih@gmail.com>
2020-04-11 16:25:01 -0500

    Added partial run demo script.

---
+++ demo/run-partial.R
@@ -0,0 +1,54 @@
+library(io)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(ggrepel)
+
+library(devtools)
+load_all();
+
+
+# Read data #################################################################
+
+genome <- "hg19";
+
+seg.luad <- qread("tcga-luad.seg");
+seg.lusc <- qread("tcga-lusc.seg");
+
+chrno <- "14";
+chrom <- paste0("chr", chrno);
+
+seg.luad.chr <- seg.luad[seg.luad$chromosome %in% chrno, ];
+seg.lusc.chr <- seg.lusc[seg.lusc$chromosome %in% chrno, ];
+
+
+# Run analysis ###############################################################
+
+# complete analysis took 15 min
+# on a single-thread of a Core i7 CPU @ 2.93GHz
+
+time.began <- proc.time();
+
+#options(mc.cores=1);
+fits <- compare_seg(seg.luad.chr, seg.lusc.chr);
+
+elapsed <- proc.time() - time.began;
+message("elasped wall time: ", elapsed[3]);
+
+
+# Examine results ############################################################
+
+# significant regions in LUAD
+regions.luad <- summary(fits);
+filter(regions.luad, end - start + 1 > 2e6, abs(ldiff) > 0.1, fdr < 0.05, n_obs > 10)
+
+qdraw(
+	{
+		with(fits$amp[["14"]],  # NKX2-1 (TFF-1) amplicon
+			plot(model, data, which=c("response", "latent", "odds"), xlab="position (Mbp)")
+		)
+	},
+	width = 5, height = 10,
+	file = "cngpld_luad_nkx2-1.pdf")
+)
+
